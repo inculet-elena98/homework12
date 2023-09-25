0667c73a5d58d76622f52fc91af413c3152d811e
Elena <inculet98@mail.ru>
2023-09-25 17:54:15 +0300

    adaugare modificari

---
+++ index.js
@@ -0,0 +1,42 @@
+// const camelCase = 'camelCase';
+// const str1 = 'Inculet'
+// const str2 = 'Elena'
+// console.log($str1 + $str2)
+
+
+// const str = 'Inculet Elena';
+// const toSnakeCase = (str = '') => {
+//   const strArr = str.split(' ');
+//   const snakeArr = strArr.reduce((acc, val) => {
+//     return acc.concat(val.toLowerCase());
+//   }, []);
+//   return snakeArr.join('_');
+// };
+// console.log(toSnakeCase(str));
+
+
+// const number = "1234"
+// console.log(number + 1)
+
+// const number = "1234"
+// console.log(number - 1)
+
+// const number = "1234"
+// console.log(number * 1)
+
+// const number = "1234"
+// console.log((number * 10) + 2)
+
+// let Number = 20 * 2;
+// let variabila = String(Number);
+// console.log(variabila)
+// console.log(typeof variabila);
+
+// let Number = 20 * 2;
+// let variabila = String(Number);
+// console.log(variabila);
+// console.log(typeof Number);
+
+var variabila = "6" / 2;
+console.log(variabila);
+console.log(typeof variabila);
